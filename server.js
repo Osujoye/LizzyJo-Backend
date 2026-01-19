7bb53617fa0d0f12173e0767f22c0913f11acb03
midestic <badmususman50@gmail.com>
2026-01-19 07:15:41 +0100

    update

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
 const PORT = process.env.PORT || 3000;
 
 const dns = require("dns");
@@ -22,18 +29,17 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// Error handler
-// app.use((err, req, res, next) => {
-//   console.error("━━━━━━ ERROR HANDLER ━━━━━━");
-//   console.error(err);
-//   const status = err.status || 500;
-//   res.status(status).json({
-//     error: {
-//       message: err.message || "Internal Server Error",
-//       ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
-//     },
-//   });
-// });
+app.use((err, req, res, next) => {
+  console.error("━━━━━━ ERROR HANDLER ━━━━━━");
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    error: {
+      message: err.message || "Internal Server Error",
+      ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
+    },
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
